dbaf8745dd2dda1a432213b74be3f792b623efba
ca-sims <csims6249@gmail.com>
2022-04-11 15:06:36 -0400

    add schema SQL.

--- Schema/Schema.sql
+++ Schema/Schema.sql
@@ -34,3 +34,23 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+
+CREATE TABLE titles (
+emp_no INT NOT NULL,
+title VARCHAR NOT NUll,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+PRIMARY KEY (emp_no)
+);
+
+CREATE TABLE dept_emp (
+emp_no INT NOT NULL,
+dept_no VARCHAR NOT NULL,
+to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+PRIMARY KEY (dept_no)
+);
+
+SELECT * FROM departments;
