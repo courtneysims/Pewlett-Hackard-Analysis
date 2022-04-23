dfefa429e0e5786c946cf5f29483ce62c9cc7ee4
ca-sims <csims6249@gmail.com>
2022-04-23 04:12:05 -0400

    add retirement queries.

--- Schema/Schema.sql
+++ Schema/Schema.sql
@@ -36,21 +36,23 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE titles (
-emp_no INT NOT NULL,
-title VARCHAR NOT NUll,
-from_date DATE NOT NULL,
-to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
+  id serial primary key,
+  emp_no INT NOT NULL,
+  title VARCHAR(40) NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
 CREATE TABLE dept_emp (
 emp_no INT NOT NULL,
 dept_no VARCHAR NOT NULL,
+from_date DATE NOT NULL,
 to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-PRIMARY KEY (dept_no)
+PRIMARY KEY (dept_no, emp_no)
 );
 
-SELECT * FROM departments;
+DROP TABLE titles CASCADE
+SELECT * FROM employees;
