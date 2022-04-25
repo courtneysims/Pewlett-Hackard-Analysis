4850b6e64255c8f837a976edbcb09556c438e07f
ca-sims <csims6249@gmail.com>
2022-04-25 02:48:19 -0400

    add query.

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -81,7 +81,27 @@ FROM employees AS e
 	AND (de.to_date = '9999-01-01')
 	ORDER BY emp_no;
 	
-
+--mentorship-eligibiliy for 1 year prior by birth date include dept.
+SELECT DISTINCT ON (emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title,
+de.dept_no,
+dp.dept_name
+INTO mentorship_eligibility_one_year
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON e.emp_no = de.emp_no
+	INNER JOIN titles AS ti
+		ON	e.emp_no = ti.emp_no
+	INNER JOIN departments AS dp
+		on de.dept_no = dp.dept_no
+	WHERE (e.birth_date BETWEEN '1964-01-01' AND '1964-12-31')
+	AND (de.to_date = '9999-01-01')
+	ORDER BY emp_no;
 
 
 Drop table mentorship_eligibility_one_year
